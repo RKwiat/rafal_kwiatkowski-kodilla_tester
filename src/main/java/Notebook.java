ee36ff90b22508675e3b7df81b76824e2951ea3e
Rafał Kwiatkowski <rafal.kwiatkowski@vp.pl>
2020-03-10 21:48:04 +0100

    pierwszy raz nie przez terminal

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,10 +1,10 @@
 public class Notebook {
     String name;
-    int weight;
+    String weight;
     int price;
     int year;
 
-    public Notebook(String name, int weight, int price, int year) {
+    public Notebook(String name, String weight, int price, int year) {
         this.name = name;
         this.weight = weight;
         this.price = price;
@@ -21,24 +21,10 @@ public class Notebook {
         }
     }
 
-    public void checkWeight() {
-        if (this.weight < 1000) {
-            System.out.println("This notebook is very light");
-        } else if (this.weight > 1000 && this.weight < 1800) {
-            System.out.println("This notebook is not too heavy");
-        } else {
-            System.out.println("This notebook is very heavy");
-        }
+    public String getWeight() {
+        return this.weight;
     }
 
-    public void checkYearPrice() {
-        if (this.year > 2018 && this.price > 1500){
-            System.out.println("This notebook is not attractive");
-        } else if (this.year > 2000 && this.price < 600) {
-            System.out.println("This notebook is attractive");
-        } else {
-            System.out.println("This notebook is very attractive");
-        }
-    }
+
 
 }
