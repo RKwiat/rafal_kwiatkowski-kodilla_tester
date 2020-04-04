06b0d75ec5768c7ca41c34c9392d5236f614b661
Rafał Kwiatkowski <rafal.kwiatkowski@vp.pl>
2020-04-04 11:21:46 +0200

    .

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Loops {
     private int i;
 
@@ -41,6 +43,18 @@ public class Loops {
         int result = 0;
         for (int i = 0; i < numbers.length; i++) {
             result = result + numbers[i];
+        }
+        return result;
+    }
+
+    public int getCountOfRandomNumber (int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
         }
         return result;
     }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,8 +2,58 @@ public class User {
     String name;
     int year;
 
+    public User(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
     public static void main(String[] args) {
 
-        String[] users = new String[]{"Man", "Womaen", "Son", "Doughter"};
+        User father = new User("father", 40);
+        User mother = new User("mother", 39);
+        User son = new User("son", 13);
+        User doughter = new User("doughter", 11);
+        User uncle = new User("uncle", 43);
+
+        User[] users = {father, mother, son, doughter, uncle};
+
+        int numberOfUsers = users.length;
+
+        int sumUsersYear = 0;
+
+        for (int i = 0; i < numberOfUsers; i++) {
+            int age = users [i].getYear();
+            sumUsersYear = sumUsersYear + age;
+        }
+        System.out.println("Suma lat uzytkowników: " + sumUsersYear);
+
+        System.out.println("Liczba użytkowników: " + numberOfUsers);
+
+        double avg = sumUsersYear / numberOfUsers;
+        System.out.println(avg);
+
+        for (int i = 0; i < numberOfUsers; i++) {
+            int age = users [i].getYear();
+            if(age < avg){
+                System.out.println(users[i].name);
+            }
+
+        }
+
+
+
     }
+
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+
+
+
+
+
 }
